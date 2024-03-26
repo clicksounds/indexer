13c061cab4969243f9a90bf276eb88ccad489e68
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-25 22:14:24 -0500

    fdsafdsafdsfafdsafdsafdsafdsafdsafsadfdsafsdsdsdsdsdsdsdsdsdsdsdsdsd

--- .github/DOWNLOAD_FILE.py
+++ .github/DOWNLOAD_FILE.py
@@ -31,6 +31,9 @@ try:
     match = re.search(r'\s*?### Add Pack\s*?(\S+)\s*?', issue_body)
     match3 = re.search(r'\s*?### Type of Click Sound\s*?(\S+)\s*?', issue_body)
     match2 = re.search(r'### Click Sound Name\s\s(.+)', issue_body)
+    print(match)
+    print(match3)
+    print(match2)
     time.sleep(2)
     if match and match2 and match3:
         clickName = match2.group(1)
