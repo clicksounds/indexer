c63e16d74389dcac968ddfc1407636933e89e429
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-03-27 21:12:08 +0000

    Update verify2.py

--- .github/verify2.py
+++ .github/verify2.py
@@ -7,9 +7,9 @@ index_path = Path(sys.argv[1])
 issue_author = os.getenv('COMMENT_AUTHOR', sys.argv[2])
 
 config = json.load(open(index_path / "indexer-config.json", "r"))
-print(f"Current User : {issue_author}")
-print("Approved users:")
-for user in config["approve"]:
-  print(user)
+#print(f"Current User : {issue_author}")
+#print("Approved users:")
+#for user in config["approve"]:
+  #print(user)
       
 print("YES" if issue_author in config["approve"] else "NO")
