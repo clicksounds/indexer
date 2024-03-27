f7ad883eec3eb7dd1cdff8adf2aa3b2d71a8dfce
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-03-27 21:02:28 +0000

    Update verify2.py

--- .github/verify2.py
+++ .github/verify2.py
@@ -6,8 +6,8 @@ index_path = Path(sys.argv[1])
 issue_author = sys.argv[2]
 
 config = json.load(open(index_path / "indexer-config.json", "r"))
-  print("Approved users:")
-  for user in config:
-      print(user)
+print("Approved users:")
+for user in config:
+  print(user)
       
 print("YES" if issue_author in config["approve"] else "NO")
