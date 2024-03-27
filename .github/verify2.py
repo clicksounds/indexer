4e0ba857d47f10c33edb825d16316aad4531fe5d
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-03-27 20:29:59 +0000

    Update verify2.py

--- .github/verify2.py
+++ .github/verify2.py
@@ -6,5 +6,4 @@ index_path = Path(sys.argv[1])
 issue_author = sys.argv[2]
 
 config = json.load(open(index_path / "indexer-config.json", "r"))
-echo("YES" if issue_author in config["approve"] else "NO")
 print("YES" if issue_author in config["approve"] else "NO")
