7708f47a7347b1d5f280d5a65cbdba0e38c34673
MuhXd <100521306+MuhXd@users.noreply.github.com>
2024-03-27 20:59:27 +0000

    APROVE

--- .github/verify2.py
+++ .github/verify2.py
@@ -1,11 +1,9 @@
 import sys
 import json
 from pathlib import Path
-try:
-    index_path = Path(sys.argv[1])
-    issue_author = sys.argv[2]
-    print(f"Arguments passed: issue_author: {issue_author} && index_path: {index_path}")
-    config = json.load(open(index_path / "indexer-config.json", "r"))
-    print("YES" if issue_author in config["approve"] else "NO")
-except Exception as e:
-    print(f"Error: {e}")
+
+index_path = Path(sys.argv[1])
+issue_author = sys.argv[2]
+
+config = json.load(open(index_path / "indexer-config.json", "r"))
+print("YES" if issue_author in config["approve"] else "NO")
